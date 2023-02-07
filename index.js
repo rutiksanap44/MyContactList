cd96125e009708908e186bb40a229114fccbfc4f
rutiksanap44 <99959986+rutiksanap44@users.noreply.github.com>
2023-02-07 14:44:38 +0530

    EJS integration started

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const path = require('path');
+
+// this app variable will have all the functionalities of Express Js
+const app = express();
+
+const port = 6204;
+
+// to use ejs template engine in our express project
+app.set('view engine', 'ejs');
+
+// to access the ejs file or file where you have embaded that code
+app.set('views',path.join(__dirname,'views'));
+
+// routes the HTTP GET Requests to the path which is being specified with the specified callback functions.
+app.get('/',function(req,res){
+    return res.render('home',{ title : 'My Contact List'});
+});
+
+app.get('/newhome',function(req,res){
+    return res.render('newHome',{ title : 'New Home'});
+});
+
+app.get('/list',function(req,res){
+    return res.render('list',{ title : 'My List'});
+})
+
+// to listen and bind the connection on the specified host and port.
+app.listen(port,function(err){
+    if(err){
+        console.log(`Error in running the Server ${err}`);
+        return;
+    }
+    console.log(`My Express Server is UP and running on Port ${port}`);
+    
+})
